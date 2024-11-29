bfe1f63bfe10631d23f73dc6e48b1785a6d16acf
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-11-28 21:38:13 -0500

    Event Form Button

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -278,7 +278,15 @@ const EventForm = ({ userId, type }: EventFormProps) => {
         </div>
 
         
-        <Button type="submit">Create Event</Button>
+        <Button 
+        type="submit"
+        size="lg"
+        disabled={form.formState.isSubmitting}
+        className="button col-span-2 w-full"
+        >
+            {form.formState.isSubmitting ? (
+                "Submitting..."
+            ): `${type} Event `}</Button>
     </form>
     </Form>
   )
